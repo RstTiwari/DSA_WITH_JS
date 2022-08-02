b7d664db5bf14a4f7149a4f96743bd41930a3d27
RstTiwari <iamrrt2121@gmail.com>
2022-08-02 09:03:39 +0530

    chapter3 added

---
+++ Chapter3_Number.js
@@ -0,0 +1,26 @@
+/**Now starting the 3rd Chaptewr for, The DSA Book */
+
+// In js 0.1 + 0.2=== 0.3  yeilds as False
+
+console.log(0.1 + 0.2 === 0.3);
+/* to really understand why 0.1 cannot be represented properly as a 32-bit floating point number,
+we must understand Binary Digit, say for  1010 represnet  10 in binary system for to evaluate 1/10 
+1/1010 = 0.0011000000000 so on ... 
+*/
+
+// JS in built Method to work around this problem
+console.log(Math.round(0.49));
+console.log(Math.ceil(0.49));
+console.log(Math.floor(0.49));
+
+Math.round(2.9); // 3
+Math.ceil(0.1); // 1
+Math.ceil(0.9); // 1
+Math.ceil(21);
+
+//Number.EPSILON
+console.log(Number.EPSILON);
+function numberisEqual(x, y) {
+  return Math.abs(x - y) < Number.EPSILON;
+}
+console.log(numberisEqual(0.2 + 0.2, 0.3));
